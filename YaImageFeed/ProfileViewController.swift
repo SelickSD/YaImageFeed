0d2c5134207b1c26b0c21a5d62cc318c168e02dd
sergey <SelickSD@Yandex.ru>
2023-04-03 20:33:14 +0300

    Сделал экран пользователя, добавил 3 лейбла, кнопку и вьюшку для фото

---
+++ YaImageFeed/ProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileViewController.swift
+//  YaImageFeed
+//
+//  Created by Сергей Денисенко on 03.04.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var profileName: UILabel!
+    @IBOutlet var loginLabel: UILabel!
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var exitButton: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
