6c3d16a9a32c4e35515bce0062c54ba626f1e866
sergey <SelickSD@Yandex.ru>
2023-04-11 21:59:34 +0300

    Сделали переходы к правильным изображениям

--- YaImageFeed/SingleImageViewController.swift
+++ YaImageFeed/SingleImageViewController.swift
@@ -8,24 +8,21 @@
 import UIKit
 
 class SingleImageViewController: UIViewController {
+
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
     
-    @IBOutlet var imageView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+        imageView.image = image
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
