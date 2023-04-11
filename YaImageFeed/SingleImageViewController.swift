127d112eb4f29cca568598c712a79eede2f25c96
sergey <SelickSD@Yandex.ru>
2023-04-11 21:19:38 +0300

    Сделал переход из ячейки в отдельный контроллер

---
+++ YaImageFeed/SingleImageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SingleImageViewController.swift
+//  YaImageFeed
+//
+//  Created by Сергей Денисенко on 11.04.2023.
+//
+
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
