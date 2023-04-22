3ae43184195edda288edb7967e2c3383c7aba1b4
sergey <SelickSD@Yandex.ru>
2023-04-22 10:13:49 +0300

    Добавил константы

---
+++ YaImageFeed/Security/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  YaImageFeed
+//
+//  Created by Сергей Денисенко on 22.04.2023.
+//
+
+import Foundation
+
+final class Constants {
+    let AccessKey: String = "mdiyQwfFaXgj71MPg000lIJGuSeT5yylpY-AhT9qNGg"
+    let SecretKey: String = "m5EhcUJqHPusWytAw73XhvwV5HzSb8skBzyTDTFzWfs"
+    let RedirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+    var DefaultBaseURL: URL {
+        guard let url = URL(string: "https://api.unsplash.com/") else {
+            preconditionFailure("Unable to construct unsplash.com")
+        }
+        return url
+    }
+    let AccessScope: String = "public+read_user+write_likes"
+}
