cabc8a8a55bcedea765a424498dcccf0634aaaf4
sergey <SelickSD@Yandex.ru>
2023-09-26 17:18:47 +0300

    Сделал новую ветку с работающего коммита, начал настройку заново. Перенес таббар

---
+++ YaImageFeed/ViewControllers/TabBarController.swift
@@ -0,0 +1,25 @@
+//
+//  TabBarController.swift
+//  YaImageFeed
+//
+//  Created by Сергей Денисенко on 26.09.2023.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil
+        )
+        self.viewControllers = [imagesListViewController, profileViewController]
+
+    }
+}
