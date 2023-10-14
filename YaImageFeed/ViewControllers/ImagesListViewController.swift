3b1d5b0a3ff0356c56517a3506059b209c13256f
sergey <SelickSD@Yandex.ru>
2023-10-14 19:41:53 +0300

    Сделал загрузку фото из сети Спринт 12_Загрузка картинок из сети

--- YaImageFeed/ViewControllers/ImagesListViewController.swift
+++ YaImageFeed/ViewControllers/ImagesListViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class ImagesListViewController: UIViewController {
 
@@ -13,20 +14,30 @@ final class ImagesListViewController: UIViewController {
 
     private let showSingleImageSegueIdentifier = "ShowSingleImage"
     private let imagesListService = ImagesListService.shared
-    private var currentPhotosCount: Int = 0
-    private let photosName: [String] = Array(0..<21).map{ "\($0)" }
+    private var imagesListServiceObserver: NSObjectProtocol?
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
         formatter.timeStyle = .none
         return formatter
     }()
+    private var photos: [Photo] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        imagesListServiceObserver = NotificationCenter.default
+            .addObserver(
+                forName: ImagesListService.DidChangeNotification,
+                object: nil,
+                queue: .main
+            ) { [weak self] _ in
+                guard let self = self else { return }
+                self.updateTableViewAnimated()
+            }
+
         DispatchQueue.main.async {
-            if self.currentPhotosCount == 0 {
+            if self.photos.count == 0 {
                 self.imagesListService.fetchPhotosNextPage()
             }
         }
@@ -37,18 +48,60 @@ final class ImagesListViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == showSingleImageSegueIdentifier {
             guard let viewController = segue.destination as? SingleImageViewController,
-                  let indexPath = sender as? IndexPath,
-                  let image = UIImage(named: photosName[indexPath.row])  else { return }
+                  let image = UIImage(named: "Stub")  else { return }
             viewController.image = image
         } else {
             super.prepare(for: segue, sender: sender)
         }
     }
 
-    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
-        guard let image = UIImage(named: photosName[indexPath.row]) else { return }
+    private func updateTableViewAnimated() {
 
-        cell.cellImage.image = image
+        let oldCount = photos.count
+        let newCount = imagesListService.photos.count
+
+        photos = imagesListService.photos
+
+        if oldCount != newCount {
+            tableView.performBatchUpdates {
+                let indexPath = (oldCount ..< newCount).map { i in
+                    IndexPath(item: i, section: 0)
+                }
+                self.tableView.insertRows(at: indexPath, with: .automatic)
+            } completion: { _ in }
+        }
+    }
+
+    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let urlToResized = URL(string: photos[indexPath.row].thumbImageURL) else {return}
+
+        let url = URL.resizedImageURL(urlToResized: urlToResized)
+        let placeholderImage = UIImage(named: "Stub")
+        let placeholderColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5)
+        let processor = RoundCornerImageProcessor(cornerRadius: 16)
+        let options: KingfisherOptionsInfo = [
+            .backgroundDecode,
+            .onFailureImage(placeholderImage?.kf.image(withBlendMode: .normal, backgroundColor: placeholderColor)),
+            .processor(processor)
+        ]
+        cell.cellImage.kf.indicatorType = .activity
+        cell.cellImage.kf.setImage(with:url,
+                                   placeholder: placeholderImage,
+                                   options: options,
+                                   completionHandler:{ [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case .success(let value):
+                // Загрузка изображения прошла успешно
+                print("Фотокарточка загружена: \(value.source.url?.absoluteString ?? "")")
+                cell.cellImage.contentMode = .scaleAspectFill
+                self?.tableView.reloadRows(at: [indexPath], with: .automatic)
+            case .failure(let error):
+                // Возникла ошибка при загрузке изображения
+                print("Фотокарточка не загружена: \(error)")
+            }
+        })
+
         cell.dataLabel.text = dateFormatter.string(from: Date())
         cell.likeButton.setImage(UIImage(named: indexPath.row % 2 != 0 ? "FavoritesActive" : "FavoritesNoActive"), for: .normal)
     }
@@ -65,7 +118,7 @@ extension ImagesListViewController: UITableViewDelegate {
 extension ImagesListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photosName.count
+        return photos.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -80,8 +133,10 @@ extension ImagesListViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        guard let image = UIImage(named: photosName[indexPath.row]) else { return CGFloat() }
 
+        guard indexPath.row < imagesListService.photos.count else { return CGFloat() }
+
+        let image = photos[indexPath.row]
         let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
         let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
         let imageWidth = image.size.width
@@ -91,6 +146,8 @@ extension ImagesListViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        // ...
+        if indexPath.row >= photos.count - 1 {
+            imagesListService.fetchPhotosNextPage()
+        }
     }
 }
